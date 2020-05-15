2d5e9b00ec567cedd220bf71eee3ec4a9f6c07b7
Jiin Jeong <33006982+jiinjeong@users.noreply.github.com>
2020-05-14 20:04:53 -0400

    Create week1.py

---
+++ PythonBeg/week1.py
@@ -0,0 +1,12 @@
+# Make a function that displays a simple text-based drawing on the screen.
+def drawing():
+    """ This draws a tree. """
+    print("       *       ")
+    print("      ***      ")
+    print("     *****     ")
+    print("       *     ")
+
+# Make a function that asks for a pet's name and greets it.
+def pet():
+    pet_name = input("What is your pet's name? ")
+    print("Hello, " + pet_name)
